0a0e4be7973118c48934a0525a1f29df4f456555
Mees van Wel <mees.vanwel@hexa-it.nl>
2025-02-08 11:22:52 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://meesvanwel.nl",
+  i18n: {
+    locales: ["nl", "en"],
+    defaultLocale: "nl",
+  },
+  integrations: [sitemap({
+    i18n: {
+      defaultLocale: "nl",
+      locales: {
+        nl: "nl-NL",
+        en: "en-US",
+      },
+    },
+  }), compressor()],
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+  nl: "Nederlands",
+  en: "English",
+};
+
+export const defaultLang = "nl";
+
+export const ui = {
+  nl: {
+    "nav.home": "Accueil",
+    "nav.about": "À propos",
+  },
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.twitter": "Twitter",
+  },
+} as const;
